731d53a15138ee57307eb4a359b8260c222372d0
yokonao <yokotukanao@me.com>
2021-06-06 22:25:40 +0900

    DBとの疎通確認

--- main.go
+++ main.go
@@ -1,9 +1,24 @@
 package main
 
 import (
+	"database/sql"
 	"fmt"
+
+	_ "github.com/go-sql-driver/mysql"
 )
 
 func main() {
-	fmt.Printf("Hello, world\n")
+	db, err := sql.Open("mysql", "root:root@tcp(localhost:3306)/tychy")
+	if err != nil {
+		panic(err)
+	}
+	defer db.Close()
+	hoge, err := db.Query("SHOW TABLES")
+	if err != nil{
+		panic(err)
+	}
+	columns, err := hoge.Columns()
+	for i := range columns{
+		println(columns[i])
+	}
 }
