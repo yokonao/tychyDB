d930719f47b12e625c358f3b1f71e0625525f61b
Ryu Yamada <a2sin2a2ko@kyoto-y.work>
2021-07-04 21:10:43 +0900

    B-Tree: insertの実装
    B-Treeにinsertの実装を追加した。
    デバッグ用に木構造をPrintする関数も作成した。

---
+++ btree/btree.go
@@ -0,0 +1,160 @@
+package main
+
+import "fmt"
+
+type Node struct {
+	degree   int
+	keys     []int
+	isLeaf   bool
+	children []*Node
+}
+
+func NewNode(degree int) *Node {
+	n := &Node{}
+	n.isLeaf = true
+	n.degree = degree
+	return n
+}
+
+func (n *Node) InsertInt(idx int, val int) {
+	n.keys = append(n.keys, 0)
+	copy(n.keys[idx+1:], n.keys[idx:])
+	n.keys[idx] = val
+}
+
+func (n *Node) InsertNode(idx int, item *Node) {
+	n.children = append(n.children, nil)
+	copy(n.children[idx+1:], n.children[idx:])
+	n.children[idx] = item
+}
+
+func (n *Node) locateLocally(x int) int {
+	for k := range n.keys {
+		if x <= n.keys[k] {
+			return k
+		}
+	}
+	return len(n.keys)
+}
+
+func (n *Node) insertRec(x int) (k int, m *Node) {
+	i := n.locateLocally(x)
+	if n.isLeaf {
+		n.InsertInt(i, x)
+	} else {
+		s, l := n.children[i].insertRec(x)
+		if l != nil {
+			n.InsertInt(i, s)
+			n.InsertNode(i, l)
+
+		}
+	}
+	if len(n.keys) < n.degree {
+		k = 0
+		m = nil
+	} else {
+		mid := len(n.keys) / 2
+		k = n.keys[mid]
+		m = NewNode(n.degree)
+		m.isLeaf = n.isLeaf
+		m.keys = make([]int, mid)
+		copy(m.keys, n.keys[:mid])
+		n.keys = n.keys[mid+1:]
+		if !n.isLeaf {
+			m.children = make([]*Node, mid+1)
+			copy(m.children, n.children[:mid+1])
+			n.children = n.children[mid+1:]
+		}
+	}
+	return
+}
+
+type BTree struct {
+	top    *Node
+	degree int
+}
+
+func NewTree(degree int) *BTree {
+	top := NewNode(degree)
+	t := &BTree{}
+	t.top = top
+	t.degree = degree
+	return t
+}
+
+func (t *BTree) insert(x int) {
+	k, m := t.top.insertRec(x)
+	if m != nil {
+		n := NewNode(t.degree)
+		n.isLeaf = false
+		n.keys = append(n.keys, k)
+		n.children = append(n.children, m, t.top)
+		t.top = n
+	}
+}
+
+func (top *Node) Print() {
+	if len(top.keys) == 0 {
+		fmt.Println("Nothing to Print")
+		return
+	}
+
+	fmt.Println("------------")
+	j := 0
+	n := 1
+	var queue []*Node
+	var height []int
+	queue = append(queue, top)
+	height = append(height, 1)
+
+	for j < n {
+		cur := queue[j]
+		h := height[j]
+		if j != 0 && h != height[j-1] {
+			fmt.Print("\n")
+		}
+		fmt.Print(cur.keys)
+		if !cur.isLeaf {
+			queue = append(queue, cur.children...)
+			s := make([]int, len(cur.children))
+			for i := range cur.children {
+				s[i] = h + 1
+				n++
+			}
+			height = append(height, s...)
+		}
+		j++
+	}
+	fmt.Print("\n")
+
+	fmt.Println("------------")
+}
+
+func main() {
+	t := NewTree(3)
+	t.insert(10)
+	t.insert(20)
+	t.insert(30)
+	t.insert(32)
+	t.insert(11)
+	t.insert(9)
+
+	t.top.Print()
+	t.insert(100)
+
+	t.top.Print()
+	t.insert(103)
+
+	t.top.Print()
+	t.insert(108)
+
+	t.top.Print()
+	t.insert(111)
+
+	t.top.Print()
+	t.insert(150)
+
+	t.top.Print()
+	t.insert(80)
+	t.top.Print()
+}
