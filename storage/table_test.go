f8dc397ac1900cd8f9236cddc07c06e9a9dd91e4
yokonao <yokotukanao@me.com>
2021-07-28 22:52:52 +0900

    :book: add test

--- storage/table_test.go
+++ storage/table_test.go
@@ -2,6 +2,7 @@ package storage_test
 
 import (
 	"os"
+	"strings"
 	"testing"
 
 	"github.com/tychyDB/storage"
@@ -78,5 +79,42 @@ func TestStorageChar(t *testing.T) {
 	countryTable.AddColumn("continent", storage.CharType(10))
 	countryTable.Add("Japan", "Asia")
 	countryTable.Add("China", "Asia")
-	countryTable.Select("name", "continent")
+	countryTable.Add("United States", "North America")
+	countryTable.Add("Russia", "Eurasia")
+	countryTable.Add("Brazil", "South America")
+	countryTable.Add("Nigeria", "Africa")
+
+	res, err := countryTable.Select("name", "continent")
+	if err != nil {
+		t.Error("failure select")
+	}
+
+	if !strings.HasPrefix(res[0][1].(string), "China") {
+		t.Errorf("expected: China, actual: %s\n", res[0][1].(string))
+	}
+	if !strings.HasPrefix(res[0][2].(string), "Japan") {
+		t.Errorf("expected: Japan, actual: %s\n", res[0][2].(string))
+	}
+	if !strings.HasPrefix(res[0][3].(string), "Nigeria") {
+		t.Errorf("expected: Nigeria, actual: %s\n", res[0][3].(string))
+	}
+
+	countryTable.Write()
+	countryTable.Read()
+	res, err = countryTable.Select("continent", "name")
+	if err != nil {
+		t.Error("failure select")
+	}
+	if !strings.HasPrefix(res[0][0].(string), "South America"){
+		t.Errorf("expected: South America, actual: %s\n", res[0][0].(string))
+	}
+	if !strings.HasPrefix(res[1][1].(string), "China") {
+		t.Errorf("expected: China, actual: %s\n", res[1][1].(string))
+	}
+	if !strings.HasPrefix(res[1][2].(string), "Japan") {
+		t.Errorf("expected: Japan, actual: %s\n", res[1][2].(string))
+	}
+	if !strings.HasPrefix(res[1][3].(string), "Nigeria") {
+		t.Errorf("expected: Nigeria, actual: %s\n", res[1][3].(string))
+	}
 }
