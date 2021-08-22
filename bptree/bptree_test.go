037e7ff36bced71e4ee596ace31c00f4d9ae50d0
Ryu Yamada <a2sin2a2ko@kyoto-y.work>
2021-08-22 16:28:02 +0900

    検索の実装

--- bptree/bptree_test.go
+++ bptree/bptree_test.go
@@ -1,6 +1,7 @@
 package bptree_test
 
 import (
+	"fmt"
 	"testing"
 
 	"github.com/tychyDB/bptree"
@@ -12,7 +13,7 @@ func TestBPTree(t *testing.T) {
 	tree.Insert(10, 1)
 	tree.Print()
 
-	tree.Insert(11, 1)
+	tree.Insert(11, 188)
 	tree.Print()
 
 	tree.Insert(12, 1)
@@ -27,15 +28,33 @@ func TestBPTree(t *testing.T) {
 	tree.Insert(1000, 1)
 	tree.Print()
 
-	tree.Insert(1, 1)
+	tree.Insert(1, 11)
 	tree.Print()
 
 	tree.Insert(2, 1)
 	tree.Print()
 
-	tree.Insert(3, 1)
+	tree.Insert(3, 11)
 	tree.Print()
 
-	tree.Insert(998, 1)
+	tree.Insert(998, 100)
 	tree.Print()
+
+	find, value := tree.Find(3)
+	fmt.Printf("key %d find %v value %d\n", 3, find, value)
+
+	find, value = tree.Find(998)
+	fmt.Printf("key %d find %v value %d\n", 998, find, value)
+
+	find, value = tree.Find(1)
+	fmt.Printf("key %d find %v value %d\n", 1, find, value)
+
+	find, value = tree.Find(11)
+	fmt.Printf("key %d find %v value %d\n", 11, find, value)
+
+	find, value = tree.Find(17)
+	fmt.Printf("key %d find %v value %d\n", 17, find, value)
+
+	find, value = tree.Find(7)
+	fmt.Printf("key %d find %v value %d\n", 7, find, value)
 }
